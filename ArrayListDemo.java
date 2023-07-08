50a4949f2ab0c56aff433a79c695b0faef856871
Krishna <dhekkrishna1@gmail.com>
2023-07-08 18:28:10 +0530

    Program to demonstrate ArrayList

---
+++ ArrayListDemo.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+       for(int i=0; i<10;i++){
+        list.add(i);
+       }
+      
+        System.out.println(list);
+       
+
+        
+    }
+    
+}
