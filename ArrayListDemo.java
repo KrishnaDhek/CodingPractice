17a641e98903b8d76b6294ddcb42d23630f8cfd0
Krishna <dhekkrishna1@gmail.com>
2023-07-08 19:31:02 +0530

    Adding some methods

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 import java.util.*;
 
 public class ArrayListDemo {
@@ -7,9 +7,34 @@ public class ArrayListDemo {
        for(int i=0; i<10;i++){
         list.add(i);
        }
-      
-        System.out.println(list);
+       //various methods 
+
+       System.out.println(list.isEmpty());
+
+    //Returns the number of elements in this list.
+        System.out.println(list.size());
+
+    //Returns true if this list contains the specified element.
+       System.out.println(list.contains(5));
+        
+    //Returns the element at the specified position in this list.
+       System.out.println(list.get(3));
+
+    //The index the first occurrence of a specific element is either returned 
+    //or -1 in case the element is not in the list.
+      System.out.println(list.indexOf(8));
+    
+    // returns true if the specified object is equal to this list
+      System.out.println(list.equals(5));
+    
+    // Removes the element at the specified position in this list. 
+    // Shifts any subsequent elements to the left (subtracts one from their indices).
+      System.out.println(list.remove(4));
+    
+    // Replaces the element at the specified position in this list with the specified element.
+      System.out.println(list.set(3,12));
        
+        System.out.println(list);
 
         
     }
