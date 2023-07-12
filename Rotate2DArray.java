897ff685d156cabb0de515216b67e09c85c6044b
Krishna <dhekkrishna1@gmail.com>
2023-07-12 15:56:07 +0530

    Clockwise Rotation

--- Rotate2DArray.java
+++ Rotate2DArray.java
@@ -34,8 +34,7 @@ public class Rotate2DArray {
             
             for(int j=0; j<arr.length; j++){
                 System.out.print(arr[i][j]+" ");
-                
-            }
+           }
             System.out.println(" ");
         }
    
