155e39bf31e5505078ca49a3c2b535a0e5c8d2e3
Krishna <dhekkrishna1@gmail.com>
2023-07-12 16:04:05 +0530

    Updated

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -3,6 +3,7 @@ public class SpiralMatrix {
     public static void printSpiral(int arr[][], int row, int column){
         int Top =0, Right = column-1, Bottom =row-1, Left=0;
 
+        System.out.println("Spiral Array");
         while(Left<=Right && Top<=Bottom){
             // Printing topmost row
             for(int i=Top; i<=Right; i++){
@@ -37,7 +38,16 @@ public class SpiralMatrix {
     public static void main(String[] args) {
         int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16,17}};
 
+        System.out.println("Original Array");
+        for(int i =0; i<arr.length; i++){
+            for(int j =0; j<arr.length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println("");
+        }
+
         printSpiral(arr,4,4);
+
 
     }
     
