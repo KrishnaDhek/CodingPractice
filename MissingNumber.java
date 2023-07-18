60b0b8c3157a1a2ea6f7c9645901ed03c2380593
krishna <dhekkrishna1@gmail.com>
2023-07-18 16:27:19 +0530

    Missing Number

---
+++ MissingNumber.java
@@ -0,0 +1,19 @@
+public class MissingNumber {
+    public static int MissingNumber(int[] arr){
+        int ans =0;
+
+        for(int res : arr){
+            ans ^= res;
+        }
+        for(int i =0; i<=arr.length; i++){
+            ans ^=i;
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {9,2,3,8,7,6,0,1,4};
+        System.out.println(MissingNumber(arr));
+    }
+    
+}
