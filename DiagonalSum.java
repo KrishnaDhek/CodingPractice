63559f4ab2deaf9f6d2de08f100bbc7f102ab685
krishna <dhekkrishna1@gmail.com>
2023-07-18 15:12:26 +0530

    2D Diagonal Sum

---
+++ DiagonalSum.java
@@ -0,0 +1,17 @@
+public class DiagonalSum {
+    public static int printSum(int[][] array){
+        int sum =0;
+        int row = array.length;
+        for(int i =0;i<row;i++){
+            sum+=array[i][i];
+        }
+        return sum;
+        
+    }
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+
+        System.out.println(printSum(arr));
+    }
+    
+}
