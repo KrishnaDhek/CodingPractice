413948ab26dd89563d19b65f41063a269ea6f204
krishna <dhekkrishna1@gmail.com>
2023-07-19 19:44:16 +0530

    Majority Element

---
+++ MajorityElement.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] a = {2,2,1,1,1,2,2};
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+
+        for(int  i=0; i<a.length; i++){
+            if(map.containsKey(a[i])){
+                map.put(a[i], map.get(a[i])+1);
+            }
+            else{
+                map.put(a[i], 1);
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> itr : map.entrySet()){
+            if(itr.getValue() > (a.length/2))
+                System.out.println(itr.getKey());
+        }
+
+    }
+    
+}
