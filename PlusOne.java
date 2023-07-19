6539a113e61802b2ae56c5e1db7a057ec0f70140
krishna <dhekkrishna1@gmail.com>
2023-07-19 19:29:30 +0530

    Plus One

---
+++ PlusOne.java
@@ -0,0 +1,25 @@
+public class PlusOne {
+    public static int[] PlusOne(int[] digits){
+
+        for(int i = digits.length-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] =0;
+        }
+
+        digits = new int[digits.length+1];
+            digits[0]=1;
+        
+        return digits;
+    }
+    public static void main(String[] args) {
+        int[] arr ={1,5,9,9,9,9,9};
+        
+        for(int i : PlusOne(arr)){
+            System.out.println(i);
+        }
+    }
+    
+}
