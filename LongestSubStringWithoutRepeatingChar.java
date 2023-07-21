4037671315325da3d8185ebb9b7240a0b609a72e
krishna <dhekkrishna1@gmail.com>
2023-07-21 18:25:29 +0530

    Longest Substring without Repeation

---
+++ LongestSubStringWithoutRepeatingChar.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class LongestSubStringWithoutRepeatingChar {
+    public static int LongestSubStringWithoutRepeatingChar(String s){
+        int start =0;
+        int maxlen =0;
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(int i =0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                while(start<i){
+                    map.remove(s.charAt(start));
+                    start++;
+
+                }
+            }
+            map.put(c,1);
+            maxlen = Math.max(maxlen, i-start+1);
+        }
+        return maxlen;
+
+    }
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(LongestSubStringWithoutRepeatingChar(s));
+    }
+    
+}
