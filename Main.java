f7c97b4efaeac03fac11cf0db6b2a0e4b39c341c
krishna <dhekkrishna1@gmail.com>
2023-07-22 17:10:20 +0530

    Circular Singly Linked List

--- Main.java
+++ Main.java
@@ -4,19 +4,35 @@ public class Main {
     SinglyLinkedList sll = new SinglyLinkedList();
     // sll.createSinglyLinkedList(2);
     // System.out.println(sll.head.value);
-    sll.insertInLinkedList(1, 0);
-    sll.insertInLinkedList(3,1 );
-    sll.insertInLinkedList(4,2);
-    sll.insertInLinkedList(5,3);
-    sll.insertInLinkedList(6, 4);
-    sll.insertInLinkedList(8, 5);
-    sll.insertInLinkedList(9, 6);
-    sll.traversLinkedList();
-    System.out.println(sll.searchNode(6));
-    sll.deletionOfNode(3);
-    sll.traversLinkedList();
-    sll.deleteSinglyLinkedList();
-    sll.traversLinkedList();
+    // sll.insertInLinkedList(1, 0);
+    // sll.insertInLinkedList(3,1 );
+    // sll.insertInLinkedList(4,2);
+    // sll.insertInLinkedList(5,3);
+    // sll.insertInLinkedList(6, 4);
+    // sll.insertInLinkedList(8, 5);
+    // sll.insertInLinkedList(9, 6);
+    // sll.traversLinkedList();
+    // System.out.println(sll.searchNode(6));
+    // sll.deletionOfNode(3);
+    // sll.traversLinkedList();
+    // sll.deleteSinglyLinkedList();
+    // sll.traversLinkedList();
+
+    CircularLinkedList csll = new CircularLinkedList();
+    csll.createCircularSinglyLinkedList(1);
+    System.out.println(csll.head.value);
+    csll.inserInCircularList(2, 0);
+    csll.inserInCircularList(4, 2);
+    csll.inserInCircularList(5, 3);
+    csll.inserInCircularList(6, 4);
+    csll.traveralCircularSinglyList();
+    System.out.println(csll.searchNode(3));
+    csll.deleteCircularList(0);
+    csll.traveralCircularSinglyList();
+    csll.deleteLinkedList();
+    csll.traveralCircularSinglyList();
+    
+
         
     }
     
