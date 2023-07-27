c9a8e684532793fc8771c877983f9629539e4172
krishna <dhekkrishna1@gmail.com>
2023-07-27 15:55:04 +0530

    second approach to find the count of difference

--- Array/CountNumberOfPairsWithAbsoluteDifferenceK.java
+++ Array/CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -4,20 +4,39 @@ import java.util.HashMap;
 
 public class CountNumberOfPairsWithAbsoluteDifferenceK {
     public static int CountKDifference(int[] nums, int k){
+
+        //solution1
+        // int count =0;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i:nums){
+        //     if(map.containsKey(i)){
+        //         map.put(i,map.get(i)+1);
+        //     }
+        //     else{
+        //         map.put(i, 1);
+        //     }
+        // }
+
+        // for(int i :nums){
+        //     if(map.containsKey(i-k)){
+        //             count +=map.get(Math.abs(i-k));
+        //     }
+        // }
+        // return count;
+
         int count =0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i:nums){
-            if(map.containsKey(i)){
-                map.put(i,map.get(i)+1);
+        int i =0;
+        int j =1;
+        while(j<nums.length){
+            if(Math.abs(nums[i]-nums[j])==k)
+                count++;
+
+            if(j+1<nums.length){
+                j++;
             }
             else{
-                map.put(i, 1);
-            }
-        }
-
-        for(int i :nums){
-            if(map.containsKey(i-k)){
-                    count +=map.get(Math.abs(i-k));
+                i++;
+                j = i+1;
             }
         }
         return count;
